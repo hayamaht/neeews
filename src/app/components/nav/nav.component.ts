c1251a1cfbca4e276b60b2e8e0e2d64333975128
hayama <hayamaroom@gmail.com>
2022-12-25 10:07:19 +0800

    Add to change country list

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { getCountryNameByAlpha2 } from 'country-locale-map';
+import { CountryCodes } from '~app/models/country-codes';
 import { getBrowserLocales, getCountryByLocale } from '~app/shared/countries-locale-map';
 
 @Component({
@@ -8,7 +10,7 @@ import { getBrowserLocales, getCountryByLocale } from '~app/shared/countries-loc
       <div class="navbar-start">
         <img src="assets/flags/1x1/{{country}}.svg"
           class="
-            w-7 h-7 rounded-full
+            w-8 h-8 rounded-full m-2
             outline outline-2 outline-offset-2
             outline-secondary
           "
@@ -26,8 +28,14 @@ import { getBrowserLocales, getCountryByLocale } from '~app/shared/countries-loc
       </div>
       <div class="navbar-end">
         <div class="dropdown dropdown-end">
-          <label tabindex="0" class="btn btn-ghost btn-circle">
-            <svg class="w-6 h-6" fill="currentColor"
+          <label tabindex="0"
+            class="
+              btn btn-sm btn-secondary btn-circle
+              text-white m-2
+              outline outline-2 outline-offset-2
+              outline-secondary
+          ">
+            <svg class="w-5 h-5 outline-" fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M7 2a1 1 0 011 1v1h3a1 1 0 110 2H9.578a18.87 18.87 0 01-1.724 4.78c.29.354.596.696.914 1.026a1 1 0 11-1.44 1.389c-.188-.196-.373-.396-.554-.6a19.098 19.098 0 01-3.107 3.567 1 1 0 01-1.334-1.49 17.087 17.087 0 003.13-3.733 18.992 18.992 0 01-1.487-2.494 1 1 0 111.79-.89c.234.47.489.928.764 1.372.417-.934.752-1.913.997-2.927H3a1 1 0 110-2h3V3a1 1 0 011-1zm6 6a1 1 0 01.894.553l2.991 5.982a.869.869 0 01.02.037l.99 1.98a1 1 0 11-1.79.895L15.383 16h-4.764l-.724 1.447a1 1 0 11-1.788-.894l.99-1.98.019-.038 2.99-5.982A1 1 0 0113 8zm-1.382 6h2.764L13 11.236 11.618 14z" clip-rule="evenodd"></path>
@@ -39,7 +47,9 @@ import { getBrowserLocales, getCountryByLocale } from '~app/shared/countries-loc
             shadow rounded
             outline outline-1 outline-gray-300
           ">
-            <li><a href="">TW</a></li>
+            <li *ngFor="let c of avaliableCountries">
+              <a href="">{{ c }}</a>
+            </li>
           </ul>
         </div>
       </div>
@@ -48,7 +58,13 @@ import { getBrowserLocales, getCountryByLocale } from '~app/shared/countries-loc
 })
 export class NavComponent implements OnInit {
 
+  // Default is United States of America
   country = 'us';
+
+  avaliableCountries: string[] = [];
+
+  countryCodes: Array<string> = Object.keys(CountryCodes)
+    .filter(k => isNaN(+k));
 
   constructor() { }
 
@@ -58,6 +74,13 @@ export class NavComponent implements OnInit {
     const country = getCountryByLocale(defaultLocale);
 
     this.country = country;
+    this.getAvaliableCountries();
+  }
+
+  getAvaliableCountries() {
+    this.avaliableCountries = this.countryCodes
+      .map(cc => getCountryNameByAlpha2(cc)!)
+      .filter(c => c !== undefined);
   }
 
 }
